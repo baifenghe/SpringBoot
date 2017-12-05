3bf5a3c257ef2c76ff81bbc363b9b450f453a007
bfh <1170126481@qq.com>
2017-12-05 08:55:45 +0800

    启用测试方法

---
+++ src/test/java/com/bfh/AppApplicationTests.java
@@ -0,0 +1,31 @@
+package com.bfh;
+
+import com.bfh.utils.MyProperties;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@EnableConfigurationProperties({MyProperties.class})
+@SuppressWarnings("all")
+public class AppApplicationTests {
+
+	@Autowired
+	private MyProperties myProperties;//这边是编译器的问题，代码可以正常运行
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Test
+	public void contextLoads() {
+		logger.info("AppApplicationTests.contextLoads()：" + myProperties.getRandomStr());
+	}
+
+
+}
