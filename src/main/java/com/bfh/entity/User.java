e09be044496faebc0dd62b55648b5d951cc50c3b
bfh <1170126481@qq.com>
2017-12-08 08:32:11 +0800

    修改User实体类

--- src/main/java/com/bfh/entity/User.java
+++ src/main/java/com/bfh/entity/User.java
@@ -6,15 +6,15 @@ package com.bfh.entity;
  */
 public class User {
 
-	private int id;
+	private Integer id;
 	private String userName;
 	private String password;
 
-	public int getId() {
+	public Integer getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
 
