82629a9f5fe8facfaa02d466625f544d64abb145
bfh <1170126481@qq.com>
2017-12-09 09:33:21 +0800

    新增测试

--- src/test/java/com/bfh/AppApplicationTests.java
+++ src/test/java/com/bfh/AppApplicationTests.java
@@ -1,6 +1,8 @@
 package com.bfh;
 
+import com.bfh.controller.IndexController;
 import com.bfh.utils.MyProperties;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -8,23 +10,45 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@EnableConfigurationProperties({MyProperties.class})
+//@EnableConfigurationProperties({MyProperties.class})
 @SuppressWarnings("all")
 public class AppApplicationTests {
 
-	@Autowired
-	private MyProperties myProperties;//这边是编译器的问题，代码可以正常运行
+	//@Autowired
+	//private MyProperties myProperties;//这边是编译器的问题，代码可以正常运行
+
+	private MockMvc mvc;
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	@Test
-	public void contextLoads() {
-		logger.info("AppApplicationTests.contextLoads()：" + myProperties.getRandomStr());
+	@Before
+	public void setUp() throws Exception {
+		mvc = MockMvcBuilders.standaloneSetup(new IndexController()).build();
 	}
+
+	@Test
+	public void getHello() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/")
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andDo(MockMvcResultHandlers.print())
+				.andReturn();
+	}
+
+//	@Test
+//	public void contextLoads() {
+//		logger.info("AppApplicationTests.contextLoads()：" + myProperties.getRandomStr());
+//	}
 
 
 }
