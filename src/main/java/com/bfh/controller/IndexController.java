c3817823140baacdbae38b5db707f31984215645
bfh <1170126481@qq.com>
2017-12-09 09:34:09 +0800

    springboot集成mybatis中使用xml配置文件

--- src/main/java/com/bfh/controller/IndexController.java
+++ src/main/java/com/bfh/controller/IndexController.java
@@ -1,10 +1,13 @@
 package com.bfh.controller;
 
+import com.bfh.entity.User;
+import com.bfh.service.UserService;
 import com.bfh.task.Task1;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -18,6 +21,8 @@ public class IndexController {
 	private Logger logger =  LoggerFactory.getLogger(this.getClass());
 	@Autowired
 	private Task1 task1;
+	@Autowired
+	private UserService userService;
 
 
 
@@ -40,5 +45,19 @@ public class IndexController {
 		task1.doTaskThree();
 
 		return "task1";
+	}
+
+
+	@RequestMapping("/save")
+	public @ResponseBody
+	User save() {
+
+		User user = new User();
+		user.setId(null);
+		user.setUserName("张三");
+		user.setPassword("123");
+
+		userService.save(user);
+		return user;
 	}
 }
